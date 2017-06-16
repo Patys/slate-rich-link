01fbeb853f45d2957c0df57b5a2f532f5dbf3f31
Patryk Szczygło <patysprog@gmail.com>
2017-06-16 15:59:24 +0200

    first working rich link

--- src/Editor.js
+++ src/Editor.js
@@ -13,64 +13,9 @@ const schema = {
 class MyEditor extends React.Component {
 
   state = {
-    state: Raw.deserialize(state, { terse: true }),
-    links: [{}]
+    state: Raw.deserialize(state, { terse: true })
   }
 
-  getPage = (href, next) => {
-    fetch('https://crossorigin.me/' + href)
-      .then((response) => {
-          return response.text();
-        }).then((data) => {
-          next(data);
-        }).catch((ex) => {
-          console.log('parsing failed', ex);
-        });
-  }
-
-  getPageInfo = (state, href) => {
-    this.getPage(href, (data) => {
-      let el = document.createElement( 'html' );
-      el.innerHTML = data;
-
-      let meta_desc = "";
-      let meta_img = "";
-
-      let metas = el.getElementsByTagName('meta');
-      for (var i=0; i<metas.length; i++) {
-        if (metas[i].getAttribute("property") == "og:description") {
-          meta_desc = metas[i].getAttribute("content");
-          // return metas[i].getAttribute("content") ? metas[i].getAttribute("content") : "Some pages";
-        }
-        if (metas[i].getAttribute("property") == "og:image") {
-          meta_img = metas[i].getAttribute("content");
-          // return metas[i].getAttribute("content") ? metas[i].getAttribute("content") : "Some pages";
-        }
-      }
-      this.setState({ links: this.state.links.concat([{"href": href, img: meta_img, desc: meta_desc, title: el.getElementsByTagName( 'title' )[0].text}])});
-    });
-
-    return state.transform().insertBlock({
-      type: 'link',
-      data: {
-        href: href,
-        title: this.getLink(href).title,
-        img: this.getLink(href).img,
-        desc: this.getLink(href).desc
-      }
-    }).focus().apply();
-  }
-
-  getLink = (href) => {
-    for(var i = 0; i < this.state.links.length; i++) {
-      if(this.state.links[i].href==href) {
-        console.log(this.state.links);
-        return this.state.links[i];
-      }
-    }
-    console.log(this.state.links);
-    return {title: "Page", img: "null", desc: "Default page description", href: href}
-  }
 
   onChange = (state) => {
     this.setState({ state })
@@ -80,9 +25,13 @@ class MyEditor extends React.Component {
     // if (state.isCollapsed) return
     if (data.type != 'text' && data.type != 'html') return
     if (!this.isUrl(data.text)) return
-    this.setState({ state });
 
-    return this.getPageInfo(state, data.text);
+    return state.transform().insertBlock({
+      type: 'link',
+      data: {
+        href: data.text
+      }
+    }).focus().apply();
   }
 
   isUrl = (url) => {
--- src/RichLink.js
+++ src/RichLink.js
@@ -1,21 +1,78 @@
 import React from 'react'
 import './RichLink.css'
 
-function RichLink(props) {
-  const { data } = props.node;
-  const href = data.get('href');
-  const title = data.get('title');
-  const desc = data.get('desc');
-  const img = data.get('img');
+class RichLink extends React.Component{
+  state = { page: {href:null}}
+  data = {}
 
-  return (
-    <div className="rich-link">
-      <div className="title">{title}</div>
-      <img width="50" height="50" src={img} alt=""/>
-      <a href={href}>{href}</a>
-      <div className="desc">{desc}</div>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    const { data } = props.node;
+    this.data = data;
+  }
+
+  componentDidMount = () => {
+    this.getPageInfo(this.data.get('href'));
+  }
+
+  getPage = (href, next) => {
+    fetch('https://crossorigin.me/' + href)
+      .then((response) => {
+          return response.text();
+        }).then((data) => {
+          next(data);
+        }).catch((ex) => {
+          console.log('parsing failed', ex);
+        });
+  }
+
+  getPageInfo = (href) => {
+    this.getPage(href, (data) => {
+      let el = document.createElement( 'html' );
+      el.innerHTML = data;
+
+      let meta_desc = "";
+      let meta_img = "";
+
+      let metas = el.getElementsByTagName('meta');
+      for (var i=0; i<metas.length; i++) {
+        if (metas[i].getAttribute("property") == "og:description") {
+          meta_desc = metas[i].getAttribute("content");
+          // return metas[i].getAttribute("content") ? metas[i].getAttribute("content") : "Some pages";
+        }
+        if (metas[i].getAttribute("property") == "og:image") {
+          meta_img = metas[i].getAttribute("content");
+          // return metas[i].getAttribute("content") ? metas[i].getAttribute("content") : "Some pages";
+        }
+      }
+      this.setState({ page: {"href": href, img: meta_img, desc: meta_desc, title: el.getElementsByTagName( 'title' )[0].text}});
+    });
+  }
+
+  // const { data } = props.node;
+  // const href = data.get('href');
+  // const title = data.get('title');
+  // const desc = data.get('desc');
+  // const img = data.get('img');
+
+  render = () => {
+    console.log(this.state.page);
+
+    if(this.state.page.href)
+      return (
+        <div className="rich-link">
+          <div className="title">{this.state.page.title}</div>
+          <img width="50" height="50" src={this.state.page.img} alt=""/>
+          <a href={this.state.href}>{this.state.page.href}</a>
+          <div className="desc">{this.state.page.desc}</div>
+        </div>
+      );
+    return (
+      <div className="rich-link">
+        <div className="title">Loading ...</div>
+      </div>
+    );
+  }
 }
 
 export default RichLink
